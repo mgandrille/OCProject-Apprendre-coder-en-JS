ccdf41e04f643ecdb8c86b73e3c9ee9e66dcccae
Marie Gandrille <57939825+mgandrille@users.noreply.github.com>
2020-01-28 12:19:13 +0100

    Finalisation du projet

--- activite_2/js/contacts.js
+++ activite_2/js/contacts.js
@@ -17,7 +17,7 @@ class Contact {
   }
   
   decrire() {
-    return `Nom : ${this.nom}   Prénom : ${this.prenom}`;
+    return `Nom : ${this.nom}    Prénom : ${this.prenom}`;
   } 
  
 }
@@ -25,34 +25,40 @@ class Contact {
 const contact1 = new Contact ("Lévisse","Carole");
 const contact2 = new Contact ("Nelsonne","Mélodie");
 
-const listeContact = [contact1, contact2];
+const listeContact = [contact1.decrire(), contact2.decrire()];
 
 console.log("Bienvenue dans le gestionnaire de contacts !");
 let i = 1;
 while (i !== 0) {
- menu();
-  const nb = Number(prompt("Choisissez une option"));
-  switch (nb) {
-    case 1 :    
-      // affiche les noms des contacts
-      console.log("Voici la liste de tous vos contacts :");
-      listeContact.forEach (contact => {
-        contact.decrire();
-      });
-      i = 1; // continue la boucle
-      break;
-    case 2 :
-      // ajoute un nom
-      const nom = String(prompt("Entrez le nom du nouveau contact :"));
-      const prenom = String(prompt("Entrez le prénom du nouveau contact :"));
-      const contact = new Contact(nom, prenom);
-      console.log("Le nouveau contact a bien été ajouté");
-      listeContact.push(contact);  // ajout dans le tableau des contacts
-      i = 1;  // continue la boucle
-      break;
-    case 0 :
-      console.log("Au revoir !");
-      i = 0;  // sort de la boucle
-      break;
-  } 
-       } 
+	console.log("\n");
+	menu();
+	console.log("\n");
+  	const nb = Number(prompt("Choisissez une option"));
+  	let boucle = nb ;
+  	while (boucle !== 0) {
+  		switch (nb) {
+  			case 1 :    
+      		// affiche les noms des contacts
+      		console.log("\nVoici la liste de tous vos contacts :");
+      		listeContact.forEach (contact => {
+	        	console.log(contact);
+      		});
+	      	break;
+    		case 2 :
+      		// ajoute un nom
+      		const nom = String(prompt("Entrez le nom du nouveau contact :"));
+      		const prenom = String(prompt("Entrez le prénom du nouveau contact :"));
+      		const contact = new Contact(nom, prenom);
+      		console.log("Le nouveau contact a bien été ajouté");
+      		listeContact.push(contact.decrire());  // ajout dans le tableau des contacts
+	      	break;
+  		}
+  		boucle = 0;
+  	}
+//  	i = 0;
+  	if (nb === 0) {
+    console.log("Au revoir !");
+   	i = 0;  // sort de la boucle
+ 	} 	
+} 	
+ 
